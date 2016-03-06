abfd4877e96c24c9ee26576928b7a0ad158081f3
Jendeberg <daniel.jendeberg@gmail.com>
2016-03-06 17:14:57 +0100

    DayController

--- controller/DayController.java
+++ controller/DayController.java
@@ -4,13 +4,19 @@ import java.net.URL;
 import java.util.Observer;
 import java.util.ResourceBundle;
 
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.util.Callback;
 import se.kth.csc.iprog.agendabuilder.model.Activity;
 import se.kth.csc.iprog.agendabuilder.model.AgendaModel;
 import se.kth.csc.iprog.agendabuilder.model.Day;
@@ -20,15 +26,15 @@ public class DayController implements Initializable, Observer {
 	private AgendaModel model;
 
 	@FXML
-	private Label startTime;
+	private TextField startTime;
 	@FXML
-	private Label endTime;
+	private TextField endTime;
 	@FXML
-	private Label label;
+	private TextField lengthTime;
 	@FXML
 	private Label colorLabel;
 	@FXML
-	private ListView<String> list;
+	private ListView<Activity> list;
 	@FXML 
 	private Button removeButton;
 
@@ -56,14 +62,75 @@ public class DayController implements Initializable, Observer {
 	}
 	
 	private void updateActivities(){
-		for(Activity act : day.activities){
-			
-		}
+		list.setItems(FXCollections.observableArrayList(day.activities));
+		updateTime();
 	}
 	
 
+	private void updateTime() {
+		startTime.setPromptText(day.getStart()/60+ ":" + (day.getStart()%60));
+		//endTime.setDisable(false);
+		//lengthTime.setDisable(false);
+		endTime.setText((day.getEnd()/60) + ":" + (day.getEnd()%60));
+		lengthTime.setText((day.getTotalLength()/60) + ":" + (day.getTotalLength()%60));
+		//endTime.setDisable(true);
+		//lengthTime.setDisable(true);
+	}
+
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		// Empty?
+		endTime.setDisable(true);
+		lengthTime.setDisable(true);
+		
+		list.setCellFactory(new Callback<ListView<Activity>, ListCell<Activity>>() {
+
+			@Override
+			public ListCell<Activity> call(ListView<Activity> p) {
+
+				ListCell<Activity> cell = new ListCell<Activity>() {
+
+					@Override
+					protected void updateItem(Activity act, boolean bln) {
+						super.updateItem(act, bln);
+						if (act != null) {
+							setText(act.toString());
+							switch (act.getType()) {
+							case Activity.PRESENTATION:
+								getStyleClass().add("pres");
+								break;
+							case Activity.GROUP_WORK:
+								getStyleClass().add("gw");
+								break;
+							case Activity.DISCUSSION:
+								getStyleClass().add("disc");
+								break;
+							case Activity.BREAK:
+								getStyleClass().add("brk");
+								break;
+							}
+						}
+					}
+				};
+				return cell;
+			}
+		});
+		
+		startTime.setOnKeyPressed(new EventHandler<KeyEvent>()
+	    {
+	        @Override
+	        public void handle(KeyEvent ke)
+	        {
+	            if (ke.getCode().equals(KeyCode.ENTER)){
+	            	System.out.println("Enter");
+	            	String[] slist = startTime.getText().split(":");
+	            	int hour = Integer.parseInt(slist[0]);
+	            	int min = Integer.parseInt(slist[1]);
+	            	if(hour>24){
+	            		day.setStart(hour*60+min);
+	            	}
+	                updateTime();
+	            }
+	        }
+	    });
 	}
 }
